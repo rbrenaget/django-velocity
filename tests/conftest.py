bc7303fc92e35c2b45ce4392ad5f7326afeab80d
Romain Brenaget <rbrenaget@gmail.com>
2026-01-29 22:04:59 +0100

    refactor(test): changed how to get model
    Signed-off-by: Romain Brenaget <rbrenaget@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,9 @@ Project-wide pytest configuration and fixtures.
 import pytest
 from rest_framework.test import APIClient
 
-from apps.users.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 @pytest.fixture
